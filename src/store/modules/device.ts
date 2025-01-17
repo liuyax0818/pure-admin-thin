9ea1166168f1ddf7cfa878a97306dbbd886cfa4b
liuyax <1803703552@qq.com>
2025-01-17 15:07:08 +0800

    docs: 补充注释

--- src/store/modules/device.ts
+++ src/store/modules/device.ts
@@ -5,6 +5,8 @@ import type { deviceInfoType } from "../types";
 export const deviceBaseInfo = defineStore({
   id: "vnet-devInfo",
   state: (): deviceInfoType => ({
+    // 此处用到bigint，打包出错
+    // Big integer literals are not available in the configured target environment ("es2015" + 2 overrides)
     baseTime: 0n
   }),
   actions: {
